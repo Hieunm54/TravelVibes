80183405541282492b0ad074fe7b3f2c8240d35a
hieunm54 <hieunm.542@gmail.com>
2024-01-15 00:02:46 +0700

    fix: change border radius

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Card = ({ children, className }) => {
   return (
     <div
-      className={`bg-white mb-3 last:mb-0 pt-3 pb-4 px-4 hover:cursor-pointer rounded-sm ${className}`}
+      className={`bg-white mb-3 last:mb-0 pt-3 pb-4 px-4 hover:cursor-pointer rounded-md ${className}`}
     >
       {children}
     </div>
